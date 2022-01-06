d7d153c386367975e091255d6f2e147e8efdadca
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-01-06 09:20:15 -0700

    add tests for @react-pdf/renderer

---
+++ src/pages/test.pdf.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+export default function RenderedDoc() {
+  return (
+    <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
+      {({ blob, url, loading, error }) =>
+        loading ? 'Loading document...' : 'Download now!'
+      }
+    </PDFDownloadLink>
+  )
+}
+
+// ReactPdf.render(<MyDocument />, `${__dirname}/example.pdf`);
+
+// export default MyDocument;
