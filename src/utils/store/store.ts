998af62ff8325e3f8fe60b691e8d102b32fd83fd
Benjamin <BensThoughts@Gmail.Com>
2022-01-12 10:45:58 -0700

    attempting a differnt redux reconciler

--- src/utils/store/store.ts
+++ src/utils/store/store.ts
@@ -10,12 +10,14 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+// import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
   blacklist: [],
+  // stateReconciler: autoMergeLevel2,
 };
 
 import formsReducer from '@app/utils/store/formsSlice';
