272185f9e63a96b0fa6de05cd6c8ecdbfc915d73
Benjamin <BensThoughts@Gmail.Com>
2022-01-12 10:47:18 -0700

    added view of finished form for testing

--- src/pages/NonEscalatorRelationship/FinishedForm.tsx
+++ src/pages/NonEscalatorRelationship/FinishedForm.tsx
@@ -1,11 +1,20 @@
 import FinishedFormLink from '@app/components/Form/FinishedFormLink';
 import GridWrapper from '../../components/GridWrapper';
 import {nonEscalatorMenu} from '@app/utils/store/questions';
+import FinishedFormPDF from '@app/components/Form/FinishedFormPDF';
+import {useAppSelector} from '@app/utils/store/hooks';
+import {formSelectors} from '@app/utils/store/formsSlice';
 
 export default function FinishedForm() {
+  const formId = nonEscalatorMenu.formId;
+  const formState = useAppSelector((state) => formSelectors.selectById(state, formId));
+  if (!formState) {
+    return null;
+  }
   return (
     <GridWrapper>
-      <FinishedFormLink formId={nonEscalatorMenu.formId} />
+      <FinishedFormLink formId={formId} />
+      <FinishedFormPDF formData={formState}/>
     </GridWrapper>
   );
 }
