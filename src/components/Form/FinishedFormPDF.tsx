17dffb733e6d78374c8e21b918be86fa74bc5912
Benjamin <BensThoughts@Gmail.Com>
2022-01-12 10:43:18 -0700

    removed N/A icon

--- src/components/Form/FinishedFormPDF.tsx
+++ src/components/Form/FinishedFormPDF.tsx
@@ -4,12 +4,13 @@ import {
   MaybeIcon,
   MustHaveIcon,
   OffLimitsIcon,
-  NAIcon,
+  // NAIcon,
 } from '@app/components/PDF/Icons';
 import {FormState} from '@app/utils/store/formsSlice';
 
 // const DEFAULT_RESPONSE_OPTIONS: string[]= ['N/A', 'Must Have', 'Like To Have', 'Maybe', 'Off Limits'];
 
+const RESPONSE_COLOR = 'purple';
 
 const styles = StyleSheet.create({
   'page': {
@@ -41,12 +42,24 @@ const styles = StyleSheet.create({
   'response': {
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'center',
+    color: RESPONSE_COLOR,
+  },
+  'completeResponse': {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
     fontSize: 8,
+    margin: '2px',
   },
   'group': {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
+    width: '100%',
+    paddingLeft: '10px',
+    paddingRight: '5px',
   },
 });
 
@@ -58,54 +71,87 @@ export default function FinishedFormPDF({
   function getIcon(value: string): React.ReactNode {
     switch (value) {
       case 'N/A':
-        return <NAIcon></NAIcon>;
+        return <></>;
+        // return <NAIcon stroke={RESPONSE_COLOR}></NAIcon>;
       case 'Must Have':
-        return <MustHaveIcon />;
+        return <MustHaveIcon stroke={RESPONSE_COLOR} />;
       case 'Like To Have':
-        return <LikeToHaveIcon />;
+        return <LikeToHaveIcon stroke={RESPONSE_COLOR} />;
       case 'Maybe':
-        return <MaybeIcon />;
+        return <MaybeIcon stroke={RESPONSE_COLOR} />;
       case 'Off Limits':
-        return <OffLimitsIcon />;
+        return <OffLimitsIcon stroke={RESPONSE_COLOR} />;
     }
   }
 
   const fieldGroupValues = Object.values(formData.fieldGroups);
-  const middleIndex = Math.ceil(fieldGroupValues.length / 2);
-  const firstHalf = fieldGroupValues.splice(0, middleIndex);
-  const secondHalf = fieldGroupValues.splice(-middleIndex);
+  const leftPage = fieldGroupValues.slice(0, 3);
+  // const middlePage = fieldGroupValues.slice(2, 4);
+  const rightPage = fieldGroupValues.slice(3, -1);
+  // const middleIndex = Math.ceil(fieldGroupValues.length / 2);
+
+  // const leftPage = fieldGroupValues.splice(0, middleIndex);
+  // const rightPage = fieldGroupValues.splice(-middleIndex);
 
   return (
     <Document>
-      <Page size="A4" style={styles.page}>
+      <Page size="LETTER" style={styles.page}>
         <View>
           <Text>{formData.formName}</Text>
         </View>
         <View style={styles.mainContainer}>
           <View style={styles.subContainer}>
-            {firstHalf.map((fieldGroup) => (
+            {leftPage.map((fieldGroup) => (
               <View key={fieldGroup.fieldGroupId} style={styles.group}>
                 <Text>{fieldGroup.fieldGroupLabel}</Text>
                 <View style={styles.group}>
                   {Object.values(fieldGroup.fields).map((field) => (
-                    <View key={field.id} style={styles.response}>
+                    <View key={field.id} style={styles.completeResponse}>
                       <Text style={styles.question}>{field.label}:&nbsp;</Text>
-                      {getIcon(field.value)}
+                      <View style={styles.response}>
+                        <Text>{field.value}</Text>
+                        {getIcon(field.value)}
+                      </View>
+                      {/* {getIcon(field.value)} */}
                     </View>
                   ))}
                 </View>
               </View>
             ))}
           </View>
-          <View style={styles.subContainer}>
-            {secondHalf.map((fieldGroup) => (
+
+          {/* <View style={styles.subContainer}>
+            {middlePage.map((fieldGroup) => (
               <View key={fieldGroup.fieldGroupId} style={styles.group}>
                 <Text>{fieldGroup.fieldGroupLabel}</Text>
                 <View style={styles.group}>
                   {Object.values(fieldGroup.fields).map((field) => (
-                    <View key={field.id} style={styles.response}>
+                    <View key={field.id} style={styles.completeResponse}>
                       <Text style={styles.question}>{field.label}:&nbsp;</Text>
-                      {getIcon(field.value)}
+                      <View style={styles.response}>
+                        <Text>{field.value}</Text>
+                        {getIcon(field.value)}
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              </View>
+            ))}
+          </View> */}
+
+          <View style={styles.subContainer}>
+            {rightPage.map((fieldGroup) => (
+              <View key={fieldGroup.fieldGroupId} style={styles.group}>
+                <Text>{fieldGroup.fieldGroupLabel}</Text>
+                <View style={styles.group}>
+                  {Object.values(fieldGroup.fields).map((field) => (
+                    <View key={field.id} style={styles.completeResponse}>
+                      <Text style={styles.question}>{field.label}:&nbsp;</Text>
+                      <View style={styles.response}>
+                        <Text>{field.value}</Text>
+                        {getIcon(field.value)}
+                      </View>
+                      {/* {getIcon(field.value)} */}
                     </View>
                   ))}
                 </View>
@@ -113,8 +159,6 @@ export default function FinishedFormPDF({
             ))}
           </View>
         </View>
-
-
       </Page>
     </Document>
   );
