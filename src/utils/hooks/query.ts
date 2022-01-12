d134fa8f8c61e5a2ecce5d3aa9a24e654109d04c
Benjamin <BensThoughts@Gmail.Com>
2022-01-12 10:46:19 -0700

    trying react-query

---
+++ src/utils/hooks/query.ts
@@ -0,0 +1,11 @@
+import {useQuery} from 'react-query';
+import {relationshipForms} from '../store/questions';
+
+export function useFormQuery(formId: string) {
+  const {isLoading, error, data} = useQuery(formId, () => {
+    return relationshipForms[formId];
+    // return new Promise<FormData>(() => relationshipForms[formId]).then((data) => data);
+  });
+
+  return [isLoading, error, data] as const;
+}
