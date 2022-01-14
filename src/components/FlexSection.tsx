23caa7ab05fff26016f4fbbc2e77b37ab9de9a63
Benjamin <BensThoughts@Gmail.Com>
2022-01-14 10:52:34 -0700

    created a component for sections in the main grid

---
+++ src/components/FlexSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FlexSection({
+  children,
+  className,
+}: React.HTMLAttributes<HTMLHtmlElement>) {
+  return (
+    <section className={`flex flex-col items-center gap-6 w-full ${className}`}>
+      {children}
+    </section>
+  );
+}
