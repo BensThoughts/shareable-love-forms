b9253f025a1307aec739ebf00d4298df01a98713
Benjamin <BensThoughts@Gmail.Com>
2022-01-14 10:53:29 -0700

    using theme colors for gradient

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,9 +9,10 @@ export default function Card({
   title,
   footer,
   children,
+  className,
 }: CardProps) {
   return (
-    <div className="p-[3px] bg-gradient-to-tr from-[#b500f2] to-[#8100f2] rounded-md shadow-md">
+    <div className={`p-[3px] bg-gradient-to-tr from-primary to-secondary rounded-md shadow-md ${className}`}>
       <div className="flex flex-col gap-2 justify-between h-full bg-app-bg-primary rounded-md p-4">
         <h2 className="font-bold tex-xl">{title}</h2>
         <span className="">
