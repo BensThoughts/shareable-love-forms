423dd98156a1dc88bced2fbdc65cbc143d4c364a
Benjamin <BensThoughts@Gmail.Com>
2022-01-17 10:04:59 -0700

    explicitly set scroll on Link to true

--- src/components/Card/CardFooter.tsx
+++ src/components/Card/CardFooter.tsx
@@ -8,7 +8,7 @@ export default function CardFooter({
 }) {
   return (
     <div className="flex w-full justify-end">
-      <Link href={href} passHref>
+      <Link href={href} passHref scroll={true}>
         <a href={href} className="">
           <span className="inline-block">goto <ChevronDoubleRightIcon className="h-5 w-5 inline-block" /></span>
         </a>
