9e6adc0bee48345e90c9e7328fc08006fcc14c9b
Benjamin <BensThoughts@Gmail.Com>
2022-01-17 10:03:38 -0700

    updated RoundedButton component

--- src/components/RoundedButton.tsx
+++ src/components/RoundedButton.tsx
@@ -1,7 +1,11 @@
-export default function RoundedButton({className, children, ...rest}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+import React from 'react';
+
+export default function RoundedButton({
+  className,
+  children,
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className={`block border-2 border-secondary border-solid rounded-md px-2 py-1 ${className}`} {...rest}>
-      {children}
-    </button>
+    <button className={`block border-2 border-secondary border-solid rounded-md px-2 py-1 ${className}`} {...rest}>{children}</button>
   );
 }
