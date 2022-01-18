c8ae501bd39ffec34a2e9e258afb56e56a109f0d
Benjamin <BensThoughts@Gmail.Com>
2022-01-18 13:47:56 -0700

    added loading spinner element

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex items-center justify-center ">
+      <div className="w-3 h-3 border-b-[2px] border-l-2 border-primary rounded-full animate-spin"></div>
+    </div>
+  );
+}
--- src/components/NonEscalatorRelationship/FinishedFormLink.tsx
+++ src/components/NonEscalatorRelationship/FinishedFormLink.tsx
@@ -2,6 +2,7 @@ import {formSelectors} from '@app/utils/store/formsSlice';
 import {useAppSelector} from '@app/utils/store/hooks';
 // import useFormCache from '@app/utils/hooks/useFormCache';
 import {PDFDownloadLink} from '@react-pdf/renderer';
+import LoadingSpinner from '../LoadingSpinner';
 import FinishedFormPDF from './FinishedFormPDF';
 
 export default function FinishedFormLink({
@@ -16,10 +17,27 @@ export default function FinishedFormLink({
   // const [formState] = useFormCache();
 
   return (
-    <PDFDownloadLink document={<FinishedFormPDF formData={formState} />} fileName="NonEscalator Relationship Menu.pdf">
-      {({blob, url, loading, error}) =>
-      loading ? 'Loading document...' : 'Download now!'
-      }
-    </PDFDownloadLink>
+    <div className="flex justify-center">
+      <PDFDownloadLink document={<FinishedFormPDF formData={formState} />} fileName="NonEscalator Relationship Menu.pdf">
+        {({blob, url, loading, error}) =>
+      loading ?
+      <div className="flex items-center justify-center border-2 border-secondary rounded-md w-[170px] h-[40px] py-1 px-2">
+        <div className="flex items-center justify-center gap-4">
+          <div className="">Loading Results</div>
+          <LoadingSpinner />
+        </div>
+      </div> :
+        <a
+          href={url as string}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="">
+          <div className="flex items-center justify-center border-2 border-secondary rounded-md w-[170px] h-[40px] py-1 px-2">
+            Download Results
+          </div>
+        </a>
+        }
+      </PDFDownloadLink>
+    </div>
   );
 }
