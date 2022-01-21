c4c0a89123ace00479dcf4dca74773ae714925f6
Benjamin <BensThoughts@Gmail.Com>
2022-01-21 14:08:18 -0700

    commented out functions

--- src/utils/hooks/query.ts
+++ src/utils/hooks/query.ts
@@ -1,11 +1,15 @@
-import {useQuery} from 'react-query';
-import {relationshipForms} from '../store/questions';
 
-export function useFormQuery(formId: string) {
-  const {isLoading, error, data} = useQuery(formId, () => {
-    return relationshipForms[formId];
-    // return new Promise<FormData>(() => relationshipForms[formId]).then((data) => data);
-  });
+export default function temp() {
 
-  return [isLoading, error, data] as const;
 }
+// import {useQuery} from 'react-query';
+// import {relationshipForms} from '../store/questions';
+
+// export function useFormQuery(formId: string) {
+//   const {isLoading, error, data} = useQuery(formId, () => {
+//     return relationshipForms[formId];
+//     // return new Promise<FormData>(() => relationshipForms[formId]).then((data) => data);
+//   });
+
+//   return [isLoading, error, data] as const;
+// }
