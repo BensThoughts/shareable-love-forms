71f2e32c5138e6c63a7fc1e208522054aaf345a2
Benjamin <BensThoughts@Gmail.Com>
2022-01-22 16:31:17 -0700

    added google tag manager script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,30 @@ class MyDocument extends Document {
   }
 
   render() {
+    const gtmHead = `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5KKGNJJ');
+    `;
+
     return (
       // <Html style={{scrollBehavior: 'smooth'}}>
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <script dangerouslySetInnerHTML={{__html: gtmHead}} />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5KKGNJJ"
+              height="0"
+              width="0"
+              style={{display: 'none', visibility: 'hidden'}}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
