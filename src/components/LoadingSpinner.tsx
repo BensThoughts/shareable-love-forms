04d6ac59e8c8fe5b2d584187ecd9cb3c430a8f8a
Benjamin <BensThoughts@Gmail.Com>
2022-01-21 20:02:33 -0700

    added size variation to loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,42 @@
-export default function LoadingSpinner() {
+type SpinnerSize = 'small' | 'medium' | 'large';
+
+export default function LoadingSpinner({
+  size = 'medium',
+  className,
+}: {
+  size?: SpinnerSize;
+  className?: string;
+}) {
+  // const bSize = String(borderSize);
+  let border = 'border-b-[2px]';
+  let length = 'w-3 h-3';
+
+  switch (size) {
+    case 'small':
+      length = 'w-3 h-3';
+      border = 'border-b-[2px]';
+      break;
+    case 'medium':
+      length = 'w-8 h-8';
+      border = 'border-b-[2px]';
+      break;
+    case 'large':
+      length = 'w-16 h-16';
+      border = 'border-b-[4px]';
+      break;
+  }
+
   return (
-    <div className="flex items-center justify-center ">
-      <div className="w-3 h-3 border-b-[2px] border-l-2 border-primary rounded-full animate-spin"></div>
+    <div className="flex justify-center items-center">
+      <div className={`rounded-full border-l-2 animate-spin ${length} ${border} border-primary ${className}`}></div>
     </div>
   );
 }
+
+// export default function LoadingSpinner() {
+//   return (
+//     <div className="flex justify-center items-center">
+//       <div className="w-3 h-3 border-b-[2px] border-l-2 border-primary rounded-full animate-spin"></div>
+//     </div>
+//   );
+// }
