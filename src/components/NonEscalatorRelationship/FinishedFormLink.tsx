6330ae1f7ba7eeb4da5e8c928b78dfc550b393a1
Benjamin <BensThoughts@Gmail.Com>
2022-01-22 19:41:18 -0700

    switched to BlobProvider

--- src/components/NonEscalatorRelationship/FinishedFormLink.tsx
+++ src/components/NonEscalatorRelationship/FinishedFormLink.tsx
@@ -2,7 +2,10 @@
 
 import {FormState} from '@app/utils/store/features/forms/formsSlice';
 // import useFormCache from '@app/utils/hooks/useFormCache';
-import {PDFDownloadLink} from '@react-pdf/renderer';
+import {
+  // PDFDownloadLink,
+  BlobProvider,
+} from '@react-pdf/renderer';
 import LoadingSpinner from '../LoadingSpinner';
 import FinishedFormPDF from './FinishedFormPDF';
 
@@ -21,7 +24,7 @@ export default function FinishedFormLink({
 
   return (
     <div className="flex justify-center">
-      <PDFDownloadLink document={<FinishedFormPDF formData={form} />} fileName="NonEscalator Relationship Menu.pdf">
+      <BlobProvider document={<FinishedFormPDF formData={form} />}>
         {({blob, url, loading, error}) =>
       loading ?
       <div className="flex items-center justify-center border-2 border-secondary rounded-md w-[170px] h-[40px] py-1 px-2">
@@ -34,13 +37,14 @@ export default function FinishedFormLink({
           href={url as string}
           target="_blank"
           rel="noreferrer noopener"
+          // download="NonEscalator Relationship Menu.pdf"
           className="">
           <div className="flex items-center justify-center border-2 border-secondary rounded-md w-[170px] h-[40px] py-1 px-2">
             Download Results
           </div>
         </a>
         }
-      </PDFDownloadLink>
+      </BlobProvider>
     </div>
   );
 }
