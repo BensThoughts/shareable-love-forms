c5ef52f6cbad4457e81c64150d1bb980bf7b09e3
Benjamin <BensThoughts@Gmail.Com>
2022-01-24 23:24:23 -0700

    using NextLink directly, and new layout TitleCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,8 @@ import FlexSection from '@app/components/FlexSection';
 import LogoSVG from '@app/components/Layout/Background/LogoSVG';
 import TitleCard from '@app/components/Card/TitleCard';
 import {ChevronDoubleRightIcon} from '@heroicons/react/solid';
-import Link from 'next/link';
+import Title from '@app/components/Title';
+import NextLink from '@app/components/NextLink';
 
 export default function Home() {
   // React.useEffect(() => {
@@ -48,26 +49,37 @@ export default function Home() {
         />
       </FlexSection>
       <FlexSection>
-        <TitleCard title="About">
+        <TitleCard
+          className="max-w-2xl rounded-md bg-neutral-dark"
+        >
+          <div className="pb-4 w-full border-b-8 border-secondary">
+            <Title className="text-neutral-lightest">About</Title>
+          </div>
+          <div className="mt-4 text-neutral-lighter">
           Sharable love forms is a website focused on forms that can help you with your love life. Pick from a form
           below, fill it out, and then share the results. Form results can be downloaded as a PDF file,
           or shared as a link.
+          </div>
+
         </TitleCard>
       </FlexSection>
       <FlexSection>
-        <Card
-          title="Non Escalator Relationship Form"
-          className="self-center max-w-md"
+        <NextLink
+          href="/non-escalator-relationship"
+          className="rounded-md"
         >
-        This is a form designed to help you think and talk about what you want in a relationship.
-          <div className="flex justify-end w-full">
-            <Link href="/non-escalator-relationship" passHref scroll={true}>
-              <a>
-                <span className="inline-block text-neutral-lightest">goto <ChevronDoubleRightIcon className="inline-block w-5 h-5" /></span>
-              </a>
-            </Link>
-          </div>
-        </Card>
+          <Card
+            title="Non Escalator Relationship Form"
+            className={`self-center max-w-md`}
+          >
+            This is a form designed to help you think and talk about what you want in a relationship.
+            <div className="flex justify-end w-full">
+              <span className="inline-block text-neutral-lightest">
+                goto <ChevronDoubleRightIcon className="inline-block w-5 h-5" />
+              </span>
+            </div>
+          </Card>
+        </NextLink>
 
       </FlexSection>
       <FlexSection>
