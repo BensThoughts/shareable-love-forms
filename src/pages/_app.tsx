529cbb9c031fb5a1ae9035c846d36e10dc5f8c35
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-11 10:12:27 -0700

    added custom domain for fathom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -75,6 +75,7 @@ function MyApp({
     //  - This must be an exact match of your domain.
     //  - If you're using www. for your domain, make sure you include that here.
     Fathom.load('ZMXBSTVH', {
+      url: 'https://ideal-great.shareloveforms.com/script.js',
       includedDomains: ['shareloveforms.com', 'www.shareloveforms.com'],
     });
 
